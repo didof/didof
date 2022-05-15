28a1b8b3c8f87e2369b8ade8204c7063a0bace76
didof <didonato.fr@gmail.com>
2022-05-15 10:18:20 +0200

    test

--- .github/actions/badges/twitter/index.js
+++ .github/actions/badges/twitter/index.js
@@ -4,13 +4,9 @@ const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 const run = require('../../_helpers/run')
 const to = require('../../_helpers/to')
 
-console.log('token', process.env.GITHUB_TOKEN)
-
 run(async () => {
     let handle = core.getInput('handle')
     if (handle.startsWith('@')) handle = handle.substring(1)
-
-    console.log(process.env.GITHUB_REPOSITORY)
 
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/")
 
@@ -30,7 +26,7 @@ run(async () => {
 
     const decoded = atob(content)
     
-    const badge = `![https://badgen.net/twitter/follow/${handle}](https://twitter.com/${handle})`
+    const badge = `\![https://badgen.net/twitter/follow/${handle}](https://twitter.com/${handle})`
     const encoded = btoa(decoded.concat(badge))
 
     // https://docs.github.com/en/rest/repos/contents#create-or-update-file-contents
